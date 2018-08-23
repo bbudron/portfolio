1012174fb93d7d436c10d05d522fa499eebbe797
bbudron <bbudron@gmail.com>
2018-08-23 18:06:44 -0400

    improved everything

--- assets/js/scrolloverflow.js
+++ assets/js/scrolloverflow.js
@@ -1667,20 +1667,20 @@
     
         if ( type === true ) {
             scrollbar.style.cssText = 'position:absolute;z-index:9999';
-            indicator.style.cssText = '-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;position:absolute;background:rgba(0,0,0,0.5);border:1px solid rgba(255,255,255,0.9);border-radius:3px';
+            indicator.style.cssText = '-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;position:absolute;background:#a5a8d2c9;border:1px solid rgba(255,255,255,0.9);border-radius:0px';
         }
     
         indicator.className = 'iScrollIndicator';
     
         if ( direction == 'h' ) {
             if ( type === true ) {
-                scrollbar.style.cssText += ';height:7px;left:2px;right:2px;bottom:0';
+                scrollbar.style.cssText += ';height:7px;left:2px;right:15px;bottom:0';
                 indicator.style.height = '100%';
             }
             scrollbar.className = 'iScrollHorizontalScrollbar';
         } else {
             if ( type === true ) {
-                scrollbar.style.cssText += ';width:7px;bottom:2px;top:2px;right:1px';
+                scrollbar.style.cssText += ';width:7px;bottom:2px;top:2px;right:15px';
                 indicator.style.width = '100%';
             }
             scrollbar.className = 'iScrollVerticalScrollbar';
